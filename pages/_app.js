b1f0e91a333c7769cbd9c8c147d22df0395d3fa3
subhan akram <subhan.akram2400@gmail.com>
2022-03-01 02:06:03 +0500

    basic setup n

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps}>SUbhan</Component>;
+}
+
+export default MyApp;
+
+// ac72edfe377c514f07cb96c6b23fce05
