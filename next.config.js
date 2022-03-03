9acfeede0849f4c38f6ed1c47795bbd7b3657e0a
subhan akram <subhan.akram2400@gmail.com>
2022-03-03 12:46:17 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// SHOPIFY_STOREFRONT_ACCESSTOKEN='ac72edfe377c514f07cb96c6b23fce05'
+// SHOPIFY_STORE_DOMAIN
