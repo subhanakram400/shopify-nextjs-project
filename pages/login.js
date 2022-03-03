84b317b1d9d6045b10ee0cf3b757043b7fd3de1e
Hashmat Ali <shahhashmatali667@gmail.com>
2022-03-04 00:44:43 +0500

    update css

--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import DraftsIcon from '@material-ui/icons/Drafts';
 import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
-import FormControl from '@material-ui/core/FormControl';
-import { InputLabel, MenuItem, Select, TextField } from '@material-ui/core';
+import '../styles/login.module.css'
+import { ownerWindow } from '@material-ui/core';
 
 
 const Login = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg" style={{
-                background: '#FFFFFF 0% 0% no-repeat padding-box',
-                boxShadow: '0px 3px 6px #00000012',
-                opacity: '1'
-            }}>
+            <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
                     <button
                         class="navbar-toggler"
@@ -31,17 +27,15 @@ const Login = () => {
                         <div>
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item active">
-                                    <a class="nav-link" aria-current="page" href="#" style={{ color: '#6B9864' }}><DraftsIcon className='mx-1' />info@ecovita.bio</a>
+                                    <a class="nav-link" aria-current="page" href="#"><DraftsIcon className='mx-1' />info@ecovita.bio</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#" style={{ color: '#6B9864' }}><PhoneIphoneIcon className='mx-1' />+43 664 4585535</a>
+                                    <a class="nav-link" href="#"><PhoneIphoneIcon className='mx-1' />+43 664 4585535</a>
                                 </li>
                                 <li class="nav-item">
-                                    {/* <a class="nav-link" href="#" style={{color: '#6B9864'}}>Pricing</a> */}
                                 </li>
                                 <li class="nav-item">
-                                    {/* <a class="nav-link" href="#" style={{color: '#6B9864'}}>About</a> */}
-                                    <button className='btn btn-success mx-2' style={{ borderRadius: '14px' }}>Neu registrieren</button>
+                                    <button className='btn btn-success mx-2'>Neu registrieren</button>
                                 </li>
                             </ul>
                         </div>
@@ -50,31 +44,21 @@ const Login = () => {
             </nav>
             <div className=''>
                 <div className='row m-0'>
-                    <div className='col-7 d-flex flex-column'>
+                    <div className='col-lg-6 align-self-center text-center'>
                         <img className="mx-auto" src='MonogramWei.png' width={'50%'} height="50%" />
                         <h1 className='mx-auto'>Hier beginnt der <br /> Handel von morgen…</h1>
                         <p className='mx-auto'>100 % digital, modern und zukunftsorientiert</p>
                     </div>
-                    <div className='col-5 d-flex flex-column'>
+                    <div className={`col-lg-6 text-center align-self-end`}>
                         <img src='LogoFarbe.png' width={'251px'} height={'84px'} className="mx-auto"/>
-                        <h1 className='mx-auto' style={{ color: '#226131', fontWeight: 'bold', fontSize: '40px' }}>Hier gehts zum Onlineshop</h1>
+                        <h1 className='mx-auto heading1'>Hier gehts zum Onlineshop</h1>
                         <h4 className='my-3 mx-auto' style={{fontSize: '29px'}}>Loggen Sie sich ein um mit Ihrer Bestellung fortzufahren</h4>
-                        <input type="email" id="form3Example3" class="form-control form-control-md mx-auto my-3 w-75"
-                            placeholder=" E-Mail" style={{
-                                background: '#FFFFFF 0% 0% no-repeat paddingBox',
-                                boxShadow: '0px 0px 8px #6b986443',
-                                borderRadius: '14px',
-                                height: '61px'
-                            }} />
+                        <input type="email" id="form3Example3" class="form-control form-control-md my-4 mx-auto my-3 w-75"
+                            placeholder=" E-Mail" />
                         <input type="email" id="form3Example3" class="form-control form-control-md my-4 mx-auto w-75"
-                            placeholder=" Password" style={{
-                                background: '#FFFFFF 0% 0% no-repeat paddingBox',
-                                boxShadow: '0px 0px 8px #6b986443',
-                                borderRadius: '14px',
-                                height: '61px'
-                            }} />
+                            placeholder=" Password" />
                         <p className='mx-auto' style={{ color: '#6B9864', fontSize: '20px' }}>Passwort vergessen</p>
-                        <button className='btn btn-success mx-auto w-75' style={{ borderRadius: '14px', height: '61px', fontSize: '30px' }}>Login</button>
+                        <button className='btn mx-auto w-75 my-2 loginBtn'>Login</button>
                     </div>
                 </div>
             </div>
